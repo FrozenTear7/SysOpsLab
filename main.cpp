673990da0deec32a5f93076e7959dbdac7abb0e1
FrozenTear7 <RainbowPablo7@gmail.com>
2018-03-06 10:38:23 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    float *arr1, *arr2;
+
+    cout << "Podaj ilosc elementow n: " << endl;
+    cin >> n;
+
+    arr1 = (float *) malloc(n * sizeof(float));
+    arr2 = (float *) calloc(n, sizeof(float));
+
+    for (int i = 0; i < n; i++) {
+        cout << arr1[i] << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << arr2[i] << endl;
+    }
+
+    free(arr1);
+    free(arr2);
+
+    return 0;
+}
