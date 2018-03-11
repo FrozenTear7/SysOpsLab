d8310afc86811f4a2c30f04d6026d46c23978455
FrozenTear <frozentear@localhost.localdomain>
2018-03-11 08:54:20 +0100

    Complete exercise 1 with makefile

--- main.c
+++ main.c
@@ -1,30 +1,10 @@
 #include <stdio.h>
 #include <time.h>
 #include "libblocks.h"
-#include <unistd.h>
-#include <sys/times.h>
-#include <sys/resource.h>
 
-void showTime(struct timeval userTime,	struct timeval systemTime, struct timespec *realTime, struct rusage *usage){
-	clock_gettime(CLOCK_REALTIME, realTime);
-	getrusage(RUSAGE_SELF, usage);
-	userTime = usage->ru_utime;
-	systemTime = usage->ru_stime;
-	printf("\nCurrent time:\nReal time:%lld\nUser time:%ld\nSystem time:%ld\n",
-		(long long) realTime->tv_sec * 1000000 + realTime->tv_nsec/1000,
-		userTime.tv_sec * 1000000 + userTime.tv_usec,
-		systemTime.tv_sec * 1000000 + systemTime.tv_usec
-	);
-}
-
-int main() { 
-	struct timespec * realTime = malloc(sizeof(struct timespec));
-	struct timeval userTime;
-	struct timeval systemTime;
-	struct rusage * usage = malloc(sizeof(struct timespec));
+int main() {
 
     printf("\nStworzenie tablicy o wielkosci 100 blokow, kazdy blok rozmiaru 100 znakow.\n");
-	showTime(userTime, systemTime, realTime, usage);
 
     int i, j, closestNumber = 500, startDelete = 10, endDelete = 15;
     unsigned int arrSize = 100, blockSize = 100;
@@ -39,16 +19,14 @@ int main() {
             tmp[j] = (char) ((rand() % 26) + 65);
         }
 
-        createBlock(testArr, tmp, i);
+        createBlock(testArr, tmp, i, blockSize);
     }
 
     printf("\nOdszukanie bloku o sumie elementow najblizszej danej liczbie.\n");
-	showTime(userTime, systemTime, realTime, usage);
 
-    char *closestArr = blockSumClosestToNumber(testArr, closestNumber);
+    char *closestArr = blockSumClosestToNumber(testArr, closestNumber, blockSize);
 
     printf("\nUsuniecie a nastepnie ponowne dodanie blokow.\n");
-	showTime(userTime, systemTime, realTime, usage);
 
     for (i = startDelete; i < endDelete; i++) {
         deleteBlock(testArr, i);
@@ -61,11 +39,10 @@ int main() {
             tmp[j] = (char) ((rand() % 26) + 65);
         }
 
-        createBlock(testArr, tmp, i);
+        createBlock(testArr, tmp, i, blockSize);
     }
 
     printf("\nNaprzemienne usuwanie i dodawanie blokow.\n");
-	showTime(userTime, systemTime, realTime, usage);
 
     for (i = startDelete; i < endDelete; i++) {
         deleteBlock(testArr, i);
@@ -75,7 +52,7 @@ int main() {
             tmp[j] = (char) ((rand() % 26) + 65);
         }
 
-        createBlock(testArr, tmp, i);
+        createBlock(testArr, tmp, i, blockSize);
     }
 
     return 0;
