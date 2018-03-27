6610039d8df78c5565a4a0f35a69dfe7e88d9eea
admin <admin@localhost.localdomain>
2018-03-28 00:09:50 +0200

    Working on restarting after 2nd sigtstp

--- cw04/zad1/main.c
+++ cw04/zad1/main.c
@@ -3,30 +3,50 @@
 #include <signal.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-void handle_sig(int signum)
+bool running = true;
+
+void exit_program()
 {
-    if (signum == SIGINT)
-        exit(1);
-    else if (signum == SIGTSTP)
-        printf("Oczekuję na CTRL+Z - kontynuacja albo CTRL+C - zakonczenie programu\n");
+    printf("Odebrano sygnał SIGINT");
+    exit(0);
+}
+
+void handle_sig()
+{
+    if (!running)
+        running = true;
+    else
+        running = false;
+
+    printf("Oczekuję na CTRL+Z - kontynuacja albo CTRL+C - zakonczenie programu\n");
+
+    struct sigaction act;
+    act.sa_handler = handle_sig;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGTSTP, &act, NULL);
 }
 
 int main()
 {
-    signal(SIGINT, handle_sig);
     struct sigaction act;
     act.sa_handler = handle_sig;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
     sigaction(SIGTSTP, &act, NULL);
+    signal(SIGINT, exit_program);
 
     while (1)
-    {
-        time_t now;
-        struct tm *now_tm;
-        now = time(NULL);
-        now_tm = localtime(&now);
+        if (running)
+        {
+            time_t now;
+            struct tm *now_tm;
+            now = time(NULL);
+            now_tm = localtime(&now);
 
-        printf("Current hour: %d:%d:%d\n", now_tm->tm_hour, now_tm->tm_min, now_tm->tm_sec);
-        sleep(1);
-    }
+            printf("Current hour: %d:%d:%d\n", now_tm->tm_hour, now_tm->tm_min, now_tm->tm_sec);
+            sleep(1);
+        }
 }
