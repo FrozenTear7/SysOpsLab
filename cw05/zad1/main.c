774037852659e1b3fe990f708555e986fcbb45a5
FrozenTear7 <pawelmendroch7@gmail.com>
2018-04-15 22:21:31 +0200

    Minor fixes in cw05

--- cw05/zad1/main.c
+++ cw05/zad1/main.c
@@ -8,14 +8,12 @@
 int maxPrograms = 10;
 int maxProgramArgv = 10;
 
-void runprogArr(char *line)
-{
+void runprogArr(char *line) {
     int progCount = 0;
     char **progArr = malloc(maxPrograms * sizeof(char *));
 
     char *curProgram = NULL;
-    while ((curProgram = strtok(curProgram == NULL ? line : NULL, "|")) != NULL)
-    {
+    while ((curProgram = strtok(curProgram == NULL ? line : NULL, "|")) != NULL) {
         if (progCount == maxPrograms)
             exit(1);
 
@@ -26,74 +24,54 @@ void runprogArr(char *line)
     for (int i = 0; i < progCount; i++)
         pids[i] = -1;
 
-    int fds[4] = {-1, -1, -1, -1};
+    int *pipes = malloc(2 * progCount * sizeof(int));
 
-    for (int i = 0; i < progCount; i++)
-    {
+    for (int j = 0; j < progCount * 2; j += 2)
+        pipe(pipes + j);
+
+    for (int i = 0; i < progCount; i++) {
         int argc = 0;
         char **argv = malloc(maxProgramArgv * sizeof(char *));
 
         char *current_arg = NULL;
-        while ((current_arg = strtok(current_arg == NULL ? progArr[i] : NULL, " \n")) != NULL)
-        {
+        while ((current_arg = strtok(current_arg == NULL ? progArr[i] : NULL, " \n")) != NULL) {
             if (argc == maxProgramArgv)
                 exit(1);
 
             argv[argc++] = current_arg;
         }
 
-        fds[1] = fds[3];
-        fds[0] = fds[2];
-
-        pipe(&fds[2]);
-
         pids[i] = fork();
-        if (pids[i] == 0)
-        {
-            if (i > 0)
-            {
-                if (fds[0] != -1)
-                {
-                    dup2(fds[0], STDIN_FILENO);
-                }
-
-                if (fds[1] != -1)
-                {
-                    close(fds[1]);
-                }
+        if (pids[i] == 0) {
+            if (i == 0) {
+                dup2(pipes[1], 1);
+            } else if (i == progCount - 1) {
+                dup2(pipes[2 * (progCount - 1) - 2], 0);
+            } else {
+                dup2(pipes[2 * (i - 1)], 0);
+                dup2(pipes[(2 * i) + 1], 1);
             }
 
-            if (i != progCount - 1)
-            {
-                if (fds[2] != -1)
-                {
-                    close(fds[2]);
-                }
-
-                if (fds[3] != -1)
-                {
-                    dup2(fds[3], STDOUT_FILENO);
-                }
+            for (int i = 0; i < progCount * 2; i++) {
+                close(pipes[i]);
             }
 
             execvp(argv[0], argv);
         }
-
-        if (fds[1] != -1)
-            close(fds[1]);
-        if (fds[0] != -1)
-            close(fds[0]);
     }
 
-    for (int i = 0; i < progCount; ++i)
+    for (int i = 0; i < progCount * 2; i++) {
+        close(pipes[i]);
+    }
+
+    for (int i = 0; i < progCount; i++)
         waitpid(pids[i], NULL, 0);
 
     free(pids);
     free(progArr);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     if (!argv[1])
         return 1;
 
