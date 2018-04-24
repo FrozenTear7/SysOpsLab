268a06c8f8cc55b80c763042f49402d90cc322f7
FrozenTear7 <pawelmendroch7@gmail.com>
2018-04-24 15:54:41 +0200

    Add ex from lab7

--- cw06/zad1/info.h
+++ cw06/zad1/info.h
@@ -4,7 +4,7 @@
 #define MAX_CLIENTS  10
 #define PROJECT_ID 1
 #define MAX_MTEXT_SIZE 50
-#define MSG_SIZE sizeof(Msg)
+#define MSG_SIZE sizeof(Msg) - sizeof(long)
 
 typedef enum mtype {
     LOGIN = 1, MIRROR = 2, ADD = 3, MUL = 4, SUB = 5, DIV = 6, TIME = 7, END = 8, INIT = 9, QUIT = 10
---
+++ cw07/labEx1/main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+
+int main(int argc, char **argv) {
+    key_t key = ftok("/main", 1);
+    int semId = semget(key, 1, IPC_CREAT | 0666);
+
+    struct sembuf *sops;
+    sops->sem_num = 0;
+    sops->sem_op = -1;
+    sops->sem_flg = IPC_NOWAIT;
+
+    semop(semId, sops, 1);
+
+    printf("%d\n", semctl(semId, 0, GETVAL));
+
+    sops->sem_op = 1;
+
+    semop(semId, sops, 1);
+
+    printf("%d\n", semctl(semId, 0, GETVAL));
+
+    semctl(semId, 1, IPC_RMID, NULL);
+
+    return 0;
+}
