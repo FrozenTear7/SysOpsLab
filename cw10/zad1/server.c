3c92dc13a7b4f3aa7997e4fc862678c1d34368fe
FrozenTear7 <pawelmendroch7@gmail.com>
2018-06-07 18:48:05 +0200

    WIP server side

--- cw10/zad1/server.c
+++ cw10/zad1/server.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/un.h>
+#include <netinet/in.h>
+#include <sys/time.h>
 #include <signal.h>
+#include <pthread.h>
 
 #define MAX_CLIENTS 14
+#define PORT 8888
+#define TRUE 1
+#define FALSE 0
 
-char *clients[MAX_CLIENTS];
+pthread_t listener, pinger, reader;
+
+int clients[MAX_CLIENTS];
 
 char *port, *unixPath;
 
@@ -25,15 +36,135 @@ void atexitHandler() {
 
 }
 
-int main(int argc, char **argv) {
-    if (argc != 3)
+void *listenerFunction() {
+    int opt = TRUE;
+    int master_socket, addrlen, new_socket, activity, valread, sd;
+    int max_sd;
+    struct sockaddr_in address;
+
+    char buffer[1025];  //data buffer of 1K
+
+    //set of socket descriptors
+    fd_set readfds;
+
+    //initialise all clients[] to 0 so not checked
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        clients[i] = 0;
+    }
+
+    //create a master socket
+    if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
         exit(1);
+    }
+
+    //set master socket to allow multiple connections ,
+    //this is just a good habit, it will work without this
+    if (setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *) &opt, sizeof(opt)) < 0) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    //type of socket created
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    //bind the socket to localhost port 8888
+    if (bind(master_socket, (struct sockaddr *) &address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(1);
+    }
+
+    //try to specify maximum of 3 pending connections for the master socket
+    if (listen(master_socket, 3) < 0) {
+        perror("listen");
+        exit(1);
+    }
+
+    //accept the incoming connection
+    addrlen = sizeof(address);
+    puts("Server ready");
+
+    while (TRUE) {
+        //clear the socket set
+        FD_ZERO(&readfds);
+
+        //add master socket to set
+        FD_SET(master_socket, &readfds);
+        max_sd = master_socket;
+
+        //add child sockets to set
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            //socket descriptor
+            sd = clients[i];
+
+            //if valid socket descriptor then add to read list
+            if (sd > 0)
+                FD_SET(sd, &readfds);
+
+            //highest file descriptor number, need it for the select function
+            if (sd > max_sd)
+                max_sd = sd;
+        }
+
+        //wait for an activity on one of the sockets , timeout is NULL ,
+        //so wait indefinitely
+        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+
+        if ((activity < 0) && (errno != EINTR)) {
+            printf("select error");
+        }
+
+        //If something happened on the master socket ,
+        //then its an incoming connection
+        if (FD_ISSET(master_socket, &readfds)) {
+            if ((new_socket = accept(master_socket, (struct sockaddr *) &address, (socklen_t * ) & addrlen)) < 0) {
+                perror("accept");
+                exit(1);
+            }
+
+            //add new socket to array of sockets
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                //if position is empty
+                if (clients[i] == 0) {
+                    clients[i] = new_socket;
+                    printf("Adding to list of sockets as %d\n", i);
+
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            sd = clients[i];
+
+            if (FD_ISSET(sd, &readfds)) {
+                if ((valread = read(sd, buffer, 1024)) == 0) {
+                    printf("Closing sd: %d\n", sd);
+                    close(sd);
+                    clients[i] = 0;
+                } else {
+                    fflush(stdin);
+                    printf("Sending to sd: %d\n", sd);
+                    send(sd, "xd", strlen("xd"), 0);
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+//    if (argc != 3)
+//        exit(1);
 
     signal(SIGINT, sigintHandler);
     atexit(atexitHandler);
 
-    port = argv[1];
-    unixPath = argv[2];
+//    port = argv[1];
+//    unixPath = argv[2];
 
-    exit(0);
+    pthread_create(&listener, NULL, listenerFunction, NULL);
+
+    while (1) {}
 }
